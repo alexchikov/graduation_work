b5e703a62e9cfbcfb0c30a60de99fc3616c0da14
alexchikov <alex.chikov@outlook.com>
2025-10-29 13:01:41 +0300

    added dag for el securities

---
+++ dags/tests/el/test__el_securities.py
@@ -0,0 +1,28 @@
+from airflow.models import DagBag
+from unittest.mock import patch
+import pytest
+
+
+DAG_ID = "el__moex_securities"
+
+@pytest.fixture(autouse=True)
+def mock_telegram_bot():
+    with patch("dags.utils.notifiers.tg.Bot") as mock_bot:
+        mock_bot.return_value = None
+        yield
+
+@pytest.fixture(autouse=True)
+def dagbag():
+    return DagBag()
+
+def test_dag_loaded(dagbag):
+    dag = dagbag.get_dag(dag_id=DAG_ID)
+    assert dagbag.import_errors == {}
+
+def test_dag_is_not_none(dagbag):
+    dag = dagbag.get_dag(dag_id=DAG_ID)
+    assert dag is not None
+
+def test_dag(dagbag):
+    dag = dagbag.get_dag(dag_id=DAG_ID)
+    assert dag.test()
---
+++ dags/utils/securities.py
@@ -0,0 +1,22 @@
+from dags.utils.cfg.configs import Config as cfg
+from botocore.client import BaseClient
+from boto3.s3.transfer import TransferConfig
+import boto3
+import requests
+
+
+def create_s3():
+    s3 = boto3.client(
+        's3',
+        endpoint_url='https://storage.yandexcloud.net',
+        aws_access_key_id=cfg.get("AWS_ACCESS_KEY"),
+        aws_secret_access_key=cfg.get("AWS_SECRET_KEY")
+    )
+    return s3
+
+def load_data(url: str, bucket: str, key: str, s3: BaseClient):
+    config = TransferConfig(multipart_threshold=50 * 1024 * 1024)
+
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        s3.upload_fileobj(r.raw, bucket, key, Config=config)
